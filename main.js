1334c6649d961a1a00948ef0b4d260aa112184d4
Thar Htet Zan <110976118+SakutaTHZ@users.noreply.github.com>
2022-10-04 17:07:48 +0630

    posts upgraded
    -removed some unnessary codes
    -need to refactor the codes

--- main.js
+++ main.js
@@ -85,13 +85,21 @@ function GenerateImages() {
 			var div = document.createElement("div");
 			div.classList.add("imageBox");
 			div.innerHTML = `
-			<img src="images/photos/photo${imageIndex}.jpg">
-			<div class="imageDetails">
-				<h3>Post Name</h3>
-				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta corrupti nemo ipsam debitis iusto ex facilis quas quia, eos possimus nam nisi fuga magni omnis. Sapiente nobis impedit alias esse.
-				</p>
-			</div>
+				<div class="uploader" title="username">
+					<img src="images/photos/photo${imageIndex}.jpg" alt="profile" class="profile">
+				</div>
+				<img src="images/photos/photo${imageIndex}.jpg">
+				<div class="imageDetails">
+					<h3>Testing the header</h3>
+					<p>by username</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta corrupti nemo ipsam debitis iusto ex facilis quas quia, eos possimus nam nisi fuga magni omnis. Sapiente nobis impedit alias esse.
+					</p>
+					<div class="reactions">
+						<button class="like upvote"><span class="goodreactCount">1k</span> Like</button>
+						<button class="like downvote"><span class="badreactCount">?</span> Dislike</button>
+					</div>
+				</div>
 			`
 			document.querySelector(`#column${index}`).appendChild(div);
 			// console.log(index)
